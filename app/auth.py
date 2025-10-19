361759747d46f1386239b669c07ef8274b34e277
Adam Kuchar <adamkuchar10@gmail.com>
2025-10-19 21:33:16 +0200

    Register commit test

--- app/auth.py
+++ app/auth.py
@@ -1,14 +1,37 @@
-from flask import Blueprint
+from flask import Blueprint, flash, redirect, url_for, render_template
+from werkzeug.security import generate_password_hash
+
+from app import db
+from app.forms import RegisterForm
+from app.models import User
+
 auth_bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 @auth_bp.route('/login')
 def login():
     return "Login Page"
 
-@auth_bp.route('/register')
+@auth_bp.route('/register', methods=['GET', 'POST'])
 def register():
-    return "Register Page"
+    form = RegisterForm()
+    if form.validate_on_submit():
+        existing_user = User.query.filter_by(email=form.email.data).first()
+        if existing_user:
+            flash('Email already registered.', 'danger')
+            return redirect(url_for('auth.register'))
 
+        new_user = User(
+            name = form.username.data,
+            email = form.email.data,
+            password = generate_password_hash(form.password.data)
+        )
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        flash('Registration successful. Please log in.', 'success')
+        return redirect(url_for('auth.login'))
+    return render_template('register.html', form=form)
 @auth_bp.route('/logout')
 def logout():
     return "Logout Page"
--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms.fields.simple import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, EqualTo
 
 
 class LoginForm(FlaskForm):
@@ -13,6 +13,7 @@ class RegisterForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password', message='Passwords must match')])
     submit = SubmitField('Register')
 
 class ExpenseForm(FlaskForm):
